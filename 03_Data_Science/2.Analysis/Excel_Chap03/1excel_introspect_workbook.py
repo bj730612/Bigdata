1fe7dfa9f451ed5744238b80726d8c1f59e979fe
USER <USER@test.com>
2018-07-13 17:05:19 +0900

    commit and push

---
+++ 03_Data_Science/2.Analysis/Excel_Chap03/1excel_introspect_workbook.py
@@ -0,0 +1,10 @@
+# 목적 : 전체 워크시트 정보 확인하기
+import sys
+from xlrd import open_workbook
+
+input_file = sys.argv[1]
+
+workbook = open_workbook(input_file)
+print("Number of worksheets:", workbook.nsheets)
+for worksheet in workbook.sheets():
+    print("Worksheet name:", worksheet.name, "\tRows:", worksheet.nrows, "\tColumns:", worksheet.ncols)
